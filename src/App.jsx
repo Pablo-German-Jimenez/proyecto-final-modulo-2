6fd5aeebd30e7f582959d5862c65e98b255fb49b
Luis Robles <luisgeremiasrobles@gmail.com>
2025-09-15 21:59:35 -0300

    Agreagamos las grillas y una imagen con titulo en la primera fila

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Footer from "./components/Footer";
+import AcercadeNosotros from "./components/AcercadeNosotros";
 
 function App() {
   return (
     <>
       <h1 className="text-success">Proyecto final modulo 2</h1>
-      <Footer></Footer>
+      <AcercadeNosotros></AcercadeNosotros>
     </>
   );
 }
---
+++ src/components/AcercadeNosotros.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const AcercadeNosotros = () => {
+  return (
+    <section className="container-fluid border border-light">
+      <div className="row">
+        <div className="col-12 col-md-12 col-lg-12 p-0 border border-light imgAcerca">
+          <h2 className="tituloAcerca">ACERCA DE NOSOTROS</h2>
+        </div>
+        <div className="col-12 col-md-6 col-lg-12 border border-light">
+          1234
+        </div>
+        <div className="col-12 col-md-6 col-lg-12 border border-light">
+          1234
+        </div>
+        <div className="col-12 col-md-6 col-lg-12 border border-light">
+          1234
+        </div>
+        <div className="col-12 col-md-6 col-lg-12 border border-light">
+          1234
+        </div>
+        <div className="col-12 col-md-6 col-lg-12 border border-light">
+          1234
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AcercadeNosotros;
