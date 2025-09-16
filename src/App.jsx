80716de14cee3b3ae5640ad0c8f0650b1675e37c
Luis Robles <luisgeremiasrobles@gmail.com>
2025-09-15 21:00:33 -0300

    Se agrego el footer y se lo adapto para los diferentes tamaños de dispositivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-function App() {
- 
+import Footer from "./components/Footer";
 
+function App() {
   return (
     <>
-        <h1 className="text-success">Proyecto final modulo 2</h1>
+      <h1 className="text-success">Proyecto final modulo 2</h1>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
